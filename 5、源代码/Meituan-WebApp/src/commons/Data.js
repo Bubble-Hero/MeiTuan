f7af72a44064598fdaf6489c70fc8f2638485f48
11184821BB <2996255147@qq.com>
2018-11-28 19:08:59 +0800

    文件覆盖第一次测试

---
+++ 5、源代码/Meituan-WebApp/src/commons/Data.js
@@ -0,0 +1,6 @@
+
+/**
+ * 获取near页面的nav数据 api
+ * @type {string}
+ */
+export const NEARNAVURL="http://localhost:3000/nearnav"
---
+++ 5、源代码/Meituan-WebApp/src/components/NearComponents/NearNav.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+      <!-----------------nearnav------------->
+      <div class="nearnav">
+        <ul>
+          <li><a href="">享美食</a></li>
+          <li><a href="">惠生活</a></li>
+          <li><a href="">爱玩乐</a></li>
+          <li><a href="">住酒店</a></li>
+          <li><a href="">全部</a></li>
+        </ul>
+      </div>
+      <!---------------nearswipernav------------>
+      <div class="nearswipernav">
+        <div class="swiper-container">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="(p,index) in nearnav.swipernavs">{{p.title}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    import Swiper from 'swiper';
+    import Apis from "../../apis/Apis";
+    export default {
+        name: "NearNav",
+        data(){
+          return{
+            nearnav:[]
+          }
+        },
+        methods:{
+          _initPageData() {
+            Apis.getNearnavData(data=>{
+              this.nearnav = data
+              console.log(this.nearnav)
+
+              this.$nextTick(()=>{
+                var swiper = new Swiper('.swiper-container', {
+                  // loop:true,
+                  spaceBetween: 30,
+                  centeredSlides: true,
+                  // autoplay: {
+                  //   delay: 2500,
+                  //   disableOnInteraction: false,
+                  // },
+                  pagination: {
+                    el: '.swiper-pagination'
+                  }
+                })
+              })
+            })
+          }
+        },
+        created(){
+          //首页加载数据
+          this._initPageData()
+        }
+    }
+</script>
+
+<style scoped>
+  @import'../../../static/css/swiper.css';
+  .nearnav ul{
+    display: flex;
+    padding: 1rem;
+    justify-content: space-around;
+  }
+  .nearnav ul li a{
+     font-size: 1.4rem;
+  }
+
+  .nearswipernav{
+    padding: 1rem;
+  }
+  .swiper-container {
+    width: 100%;
+    height: 100%;
+  }
+  .swiper-slide {
+    text-align: center;
+    font-size: 12px;
+    background: #fff;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+  }
+  .swiper-wrapper{
+    display: flex;
+  }
+  .navtitle{
+    display: block;
+    padding: 0 2rem;
+  }
+</style>
